d5abc07b962577e09f77126bd1832071a85b87d9
ttining <ttining.real@gmail.com>
2025-01-13 23:43:00 +0900

    [Day 1] 폴더 구조 변경 및 README.md 추가
    - 나머지 구하기
    - 몫 구하기

---
+++ Day 1. 나머지 구하기/index.js
@@ -0,0 +1,13 @@
+// 나머지 구하기
+
+function solution(num1, num2) {
+  const result = num1 % num2;
+
+  return result;
+}
+
+solution(3, 2);
+solution(10, 5);
+
+// console.log(solution(3, 2)); // 1
+// console.log(solution(10, 5)); // 0
---
+++ Day 2. 몫 구하기/index.js
@@ -0,0 +1,13 @@
+// 몫 구하기
+
+function solution(num1, num2) {
+  const result = Math.floor(num1 / num2);
+
+  return result;
+}
+
+solution(10, 5);
+solution(7, 2);
+
+// console.log(solution(10, 5)); // 2
+// console.log(solution(7, 2)); // 3
