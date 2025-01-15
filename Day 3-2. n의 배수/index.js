6bef45b52c3c0a8a167883d2a4340f9da18f8a3a
ttining <ttining.real@gmail.com>
2025-01-15 11:50:42 +0900

    [Day 3]
    - 각도기
    - n의 배수

---
+++ Day 3-2. n의 배수/index.js
@@ -0,0 +1,12 @@
+// n의 배수
+const solution = (num, n) => {
+  // do something
+  if (num % n === 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+console.log(solution(98, 2)); // 1
+console.log(solution(34, 3)); // 0
