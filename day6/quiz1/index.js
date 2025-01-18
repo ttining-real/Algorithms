61ba69dfafccba58729d33caac2bb6a8d87c8d9e
ttining <ttining.real@gmail.com>
2025-01-18 16:31:06 +0900

    [수정] README.md 수정
    - README.md 수정
    - day6 문제 추가

---
+++ day6/quiz1/index.js
@@ -0,0 +1,8 @@
+// 더 크게 합치기
+
+const solution = (a, b) => {
+  // do something
+};
+
+console.log(solution(9, 91)); // 991
+console.log(solution(89, 8)); // 898
---
+++ day6/quiz2/index.js
@@ -0,0 +1,8 @@
+// 두 수의 연산값 비교하기
+
+const solution = (a, b) => {
+  // do something
+};
+
+console.log(solution(2, 91)); // 364
+console.log(solution(91, 2)); // 912
